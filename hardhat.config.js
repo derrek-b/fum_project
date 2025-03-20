9cd989710e7aaf1a3d76e6f032e33b26b357b9a5
derrek-b <derrek.brack@protonmail.com>
2025-03-20 13:13:00 -0600

    Set up .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+      }
+    }
+  }
 };
